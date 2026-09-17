8ed0b2f0097fb2ca3705e3d62d775f150f0410f8
samriddhi687 <samriddhi19772@gmail.com>
2026-09-17 18:49:00 +0530

    commit

--- SourceCode/com/customeranalytics/model/Feedback.java
+++ SourceCode/com/customeranalytics/model/Feedback.java
@@ -1,24 +1,22 @@
 package com.customeranalytics.model;
-public class Feedback {
+public class Feedback {  //feedback is linked to the customer 
     private int customerId;
     private int rating;
     private String comment;
 
-    public Feedback(int customerId, int rating, String comment){
+    public Feedback(int customerId, int rating, String comment) {  //constructor
         this.customerId = customerId;
         this.rating = rating;
         this.comment = comment;
     }
 
-    public int getCustomerId(){
+    public int getCustomerId() {
         return customerId;
     }
-
-    public int getRating(){
+    public int getRating() {
         return rating;
     }
-
-    public String getComment(){
+    public String getComment() {
         return comment;
     }
 }
