ad5fb182fc066a6f5beeb0e5c52eb8876079c14d
samriddhi687 <samriddhi19772@gmail.com>
2026-09-17 19:00:29 +0530

    update CustomerService.java

--- SourceCode/com/customeranalytics/service/CustomerService.java
+++ SourceCode/com/customeranalytics/service/CustomerService.java
@@ -1,33 +1,32 @@
 package com.customeranalytics.service;
 import com.customeranalytics.model.Customer;
-import java.util.ArrayList;
 
+import java.util.ArrayList;
 public class CustomerService {
-    private ArrayList<Customer> customers = new ArrayList<>();
-    public void addCustomer(Customer customer) {
+    private ArrayList<Customer> customers = new ArrayList<>();  //stores all the customers added to the system
+
+    public void addCustomer(Customer customer) {  //adds new customer
         customers.add(customer);
         System.out.println("Customer added successfully.");
     }
 
-    public void viewCustomers() {
-
+    public void viewCustomers() {  //displays all customers currently stored
         if (customers.isEmpty()) {
             System.out.println("No customers found.");
             return;
         }
-
         for (Customer customer : customers) {
-            System.out.println("-------------------------");
+            System.out.println("---------------------------------");
             customer.displayCustomer();
         }
     }
-
+   
     public Customer findCustomer(int customerId) {
         for (Customer customer : customers) {
-            if (customer.getCustomerId() == customerId) {
+            if (customer.getCustomerId() == customerId) {  //id used
                 return customer;
             }
         }
-        return null;
+        return null;  //if no customer with the given ID is found
     }
 }
