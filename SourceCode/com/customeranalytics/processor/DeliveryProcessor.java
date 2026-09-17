f26f974c4fc0f17c5121d895f242347fbc1f24b9
samriddhi687 <samriddhi19772@gmail.com>
2026-09-17 18:53:31 +0530

    c

--- SourceCode/com/customeranalytics/processor/DeliveryProcessor.java
+++ SourceCode/com/customeranalytics/processor/DeliveryProcessor.java
@@ -1,17 +1,15 @@
 package com.customeranalytics.processor;
 import com.customeranalytics.model.Complaint;
+//handles complaints related to delivery
 public class DeliveryProcessor extends ComplaintProcessor {
-    @Override
+    @Override   // Inheritance
     public void processComplaint(Complaint complaint) {
-        if (complaint.getIssue().equals("Package Not Received")
-                || complaint.getIssue().equals("Package Damaged")) {
-
+        if (complaint.getIssue().equals("Package not received") || complaint.getIssue().equals("Package damaged")) {
             setPriority(complaint, "High");
         } else {
             setPriority(complaint, "Normal");
         }
         complaint.setStatus("In Progress");
-
         System.out.println("Delivery complaint processed.");
         System.out.println("Priority: " + complaint.getPriority());
     }
