626ae63edb1a09452b0c7aca86d6504a05114544
samriddhi687 <samriddhi19772@gmail.com>
2026-09-17 18:55:04 +0530

    Improve comments
    Updated comments for clarity in GeneralProcessor class.

--- SourceCode/com/customeranalytics/processor/GeneralProcessor.java
+++ SourceCode/com/customeranalytics/processor/GeneralProcessor.java
@@ -1,11 +1,11 @@
 package com.customeranalytics.processor;
 import com.customeranalytics.model.Complaint;
-public class GeneralProcessor extends ComplaintProcessor {
+
+public class GeneralProcessor extends ComplaintProcessor {  //handles complaints that do not need a category-specific processor
     @Override
     public void processComplaint(Complaint complaint) {
-        setPriority(complaint, "Normal");
+        setPriority(complaint, "Normal");  //priority set to Normal.
         complaint.setStatus("In Progress");
-
         System.out.println("Complaint processed.");
         System.out.println("Priority: " + complaint.getPriority());
     }
