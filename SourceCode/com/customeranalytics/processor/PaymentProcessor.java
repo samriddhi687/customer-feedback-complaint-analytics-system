d4fd4eb7d3e92940009c4fa9a7291f99cdbb6f73
samriddhi687 <samriddhi19772@gmail.com>
2026-09-17 18:57:11 +0530

    update

--- SourceCode/com/customeranalytics/processor/PaymentProcessor.java
+++ SourceCode/com/customeranalytics/processor/PaymentProcessor.java
@@ -1,16 +1,16 @@
 package com.customeranalytics.processor;
 import com.customeranalytics.model.Complaint;
-public class PaymentProcessor extends ComplaintProcessor {
-    @Override
+
+public class PaymentProcessor extends ComplaintProcessor {  //complaints related to payments
+    @Override  //inheritacne
     public void processComplaint(Complaint complaint) {
-        if (complaint.getIssue().equals("Payment Failed")
-                || complaint.getIssue().equals("Amount Deducted but Order Failed")) {
+        //payment failures can affect an order directly,so these are given higher priority.
+        if (complaint.getIssue().equals("Payment Failed") || complaint.getIssue().equals("Amount Deducted but Order Failed")) {
             setPriority(complaint, "High");
         } else {
             setPriority(complaint, "Normal");
         }
         complaint.setStatus("In Progress");
-
         System.out.println("Payment complaint processed.");
         System.out.println("Priority: " + complaint.getPriority());
     }
