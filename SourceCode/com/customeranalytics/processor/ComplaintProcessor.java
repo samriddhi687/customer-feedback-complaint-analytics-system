9c09f3e6494eee473859dd58e8ed7517c0ba2624
samriddhi687 <samriddhi19772@gmail.com>
2026-09-17 18:51:06 +0530

    Update ComplaintProcessor.java

--- SourceCode/com/customeranalytics/processor/ComplaintProcessor.java
+++ SourceCode/com/customeranalytics/processor/ComplaintProcessor.java
@@ -1,9 +1,10 @@
 package com.customeranalytics.processor;
 import com.customeranalytics.model.Complaint;
-    public abstract class ComplaintProcessor {
-        public abstract void processComplaint(Complaint complaint);
-        protected void setPriority(Complaint complaint, String priority) {
-            complaint.setPriority(priority);
-        }
-    }
 
+public abstract class ComplaintProcessor {  //Parent class
+    public abstract void processComplaint(Complaint complaint);
+
+    protected void setPriority(Complaint complaint, String priority) {
+        complaint.setPriority(priority);
+    }
+}
