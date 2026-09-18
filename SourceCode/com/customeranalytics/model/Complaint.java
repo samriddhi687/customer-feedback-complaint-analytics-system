3becc5e1f62c0fe251a76727d8369d3972ed07c2
samriddhi687 <samriddhi19772@gmail.com>
2026-09-18 19:46:28 +0530

    commit

--- SourceCode/com/customeranalytics/model/Complaint.java
+++ SourceCode/com/customeranalytics/model/Complaint.java
@@ -1,34 +1,64 @@
 package com.customeranalytics.model;
-public class Customer {  //basic information about the customer
+public class Complaint {
+    private int complaintId;
     private int customerId;
-    private String name;
-    private String email;
-    private String phone;
+    private String category;
+    private String issue;
 
-    public Customer(int customerId, String name, String email, String phone) {  //constructor used to create a new customer
+    private String status;
+    private String priority;
+    private double resolutionTime;
+
+    public Complaint(int complaintId, int customerId, String category, String issue) {
+        this.complaintId = complaintId;
         this.customerId = customerId;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-    }
+        this.category = category;
+        this.issue = issue;
 
-    public int getCustomerId() {  //to access customer details
+        status = "Pending";
+        priority = "Normal";
+        resolutionTime = 0;
+    }
+    // getters
+    public int getComplaintId() {
+        return complaintId;
+    }
+    public int getCustomerId() {
         return customerId;
     }
-    public String getName() {
-        return name;
+    public String getCategory() {
+        return category;
     }
-    public String getEmail() {
-        return email;
+    public String getIssue() {
+        return issue;
     }
-    public String getPhone() {
-        return phone;
+    public String getStatus() {
+        return status;
+    }
+    public String getPriority() {
+        return priority;
+    }
+    public double getResolutionTime() {
+        return resolutionTime;
+    }
+    // Setters
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public void setPriority(String priority) {  //priority assignment
+        this.priority = priority;
+    }
+    public void setResolutionTime(double resolutionTime) {  // time-taken for resolvment
+        this.resolutionTime = resolutionTime;
     }
 
-    public void displayCustomer() {
+    public void displayComplaint() {  //displays the current information of the complaint
+        System.out.println("Complaint ID: " + complaintId);
         System.out.println("Customer ID: " + customerId);
-        System.out.println("Name: " + name);
-        System.out.println("Email: " + email);
-        System.out.println("Phone: " + phone);
+        System.out.println("Category: " + category);
+        System.out.println("Issue: " + issue);
+        System.out.println("Status: " + status);
+        System.out.println("Priority: " + priority);
+        System.out.println("Resolution Time: " + resolutionTime + " hours");
     }
 }
